d6c4fc7f04f832f58341a81063be8c6348791ce4
thomastangweixin@163.com <6L7k5VfUPLEcE58C>
2019-05-12 15:38:24 +0800

    1.0.3: 升级咯

--- release.sh
+++ release.sh
@@ -4,7 +4,11 @@
 echo "切换到当前目录"
 cd "$(dirname "$0")"
 
+#签名失败
+#gpg --list-secret-keys
+#gpg --keyserver keyserver.ubuntu.com --send-keys 7D6B64B07C0075B5
 
-mvn -s ~/.m2/settings.sonatype.xml clean deploy verify  -Dgpg.passphrase=hDKFUo6b
+mvn -s ~/.m2/settings.sonatype.xml clean deploy verify
+#-Dgpg.passphrase=hDKFUo6b
 
 echo "完成"
